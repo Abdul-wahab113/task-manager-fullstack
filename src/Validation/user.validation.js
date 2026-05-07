39ea2567ce4a8121019287616d2f9562df238a92
Buddy Wick <wahabahmad800@gmail.com>
2026-05-08 01:55:44 +0500

    feat(auth): add zod validation schema for user registration

---
+++ src/Validation/user.validation.js
@@ -0,0 +1,24 @@
+import { z } from "zod";
+
+const userRegistionSchemaValidation = z.object({
+    username: z.string()
+        .trim()
+        .lowercase()
+        .min(3, "Username must be 3 character long")
+        .max(20, "Too long username"),
+
+    email: z.string()
+        .trim()
+        .lowercase()
+        .email({ message: "Please enter a valid email" })
+        .min(5, "Email is too short")
+        .max(100, "Email is too long"),
+
+    password: z.string()
+        .min(8, "Password must be 8 characters long")
+        .max(100, "Too long password")
+        .regex(/[A-Z]/, "Must contain one upper-case letter")
+        .regex(/[a-z]/, "Must contain one lower-case letter")
+        .regex(/[0-9]/, "Must contain a digit")
+
+});
