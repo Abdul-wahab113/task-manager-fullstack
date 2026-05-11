9307e7d7b5afae1d9ac6ce72a8aa589c10978f87
Buddy Wick <wahabahmad800@gmail.com>
2026-05-12 00:28:27 +0500

    feat: configure CORS for frontend integration and security

--- src/app.js
+++ src/app.js
@@ -1,4 +1,6 @@
 import express from "express";
+import cors from "cors";
+import "dotenv/config";
 import userRoutes from "./Routes/users.routes.js";
 import taskRoutes from "./Routes/tasks.routes.js";
 import { ApiError } from "./Utils/api.error.utils.js";
@@ -8,7 +10,22 @@ const app = express();
 
 // middleware
 app.use(express.json({ limit: "16kb" }));
+app.use(express.urlencoded({ extended: true }));
 
+// CORS Configuration
+// CORS (Cross-Origin Resource Sharing) is a security feature implemented by web browsers to 
+// restrict web pages from making requests to a different domain than the one that served the web page.
+app.use(cors({
+    // allow the frontend to access the backend resources from this origin
+    origin: process.env.CORS_ORIGIN?.split(",") || "http://localhost:5173",
+
+    credential: true,
+    // allow the frontend to send cookies and auth headers with the request
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTION"],
+
+    // allow the frontend to send these headers in the request
+    headers: ["Content-Type", "Authorization"]
+}));
 
 //routes
 app.use("/api/v1/users", userRoutes);
